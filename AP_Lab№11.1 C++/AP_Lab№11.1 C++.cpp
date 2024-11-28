5c047bf4e3ebf80708f72d85ada6aa9d9e24b04b
DenisSavytskiy <denys.savytskyi.ri.2024@lpnu.ua>
2024-11-28 05:06:06 +0200

    first commit

---
+++ AP_Lab№11.1 C++/AP_Lab№11.1 C++.cpp
@@ -0,0 +1,57 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+#include <fstream>
+#include <string>
+using namespace std;
+
+// Функція для виведення вмісту файлу
+void DisplayFileContent(const string& filename) {
+    ifstream inputFile(filename);
+    if (!inputFile) {
+        cout << "Error opening file: " << filename << endl;
+        return;
+    }
+
+    string line;
+    while (getline(inputFile, line)) {
+        cout << line << endl;
+    }
+    inputFile.close();
+}
+
+// Функція підрахунку входжень послідовності "while"
+int CountWhileOccurrences(const string& filename) {
+    ifstream inputFile(filename);
+    if (!inputFile) {
+        cout << "Error opening file: " << filename << endl;
+        return 0;
+    }
+
+    int whileCount = 0;
+    string line;
+    while (getline(inputFile, line)) {
+        size_t pos = 0;
+        while ((pos = line.find("while", pos)) != string::npos) {
+            whileCount++;
+            pos += 5;  // довжина "while"
+        }
+    }
+    inputFile.close();
+    return whileCount;
+}
+
+int main() {
+    string filename;
+    cout << "Enter filename: ";
+    cin >> filename;
+
+    // Виведення вмісту файлу
+    cout << "\nFile contents:\n";
+    DisplayFileContent(filename);
+
+    // Підрахунок входжень "while"
+    int occurrences = CountWhileOccurrences(filename);
+    cout << "\nNumber of 'while' occurrences: " << occurrences << endl;
+
+    return 0;
+}
---
+++ UnitTest1/UnitTest1.cpp
@@ -0,0 +1,59 @@
+﻿#include "pch.h"
+#include "CppUnitTest.h"
+#include <fstream>
+#include <string>
+#include "../AP_Lab№11.1 C++/AP_Lab№11.1 C++.cpp"
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace UnitTest1
+{
+    TEST_CLASS(UnitTest1)
+    {
+    public:
+        // Тестування на файлі з відомою кількістю входжень "while"
+        TEST_METHOD(TestWhileOccurrencesInFile)
+        {
+            // Створюємо тестовий файл
+            std::string testFilename = "test_while_file.txt";
+            std::ofstream testFile(testFilename);
+            testFile << "This is a test while file\n";
+            testFile << "while testing is important\n";
+            testFile << "Another while test case\n";
+            testFile.close();
+
+            // Перевіряємо очікувану кількість входжень
+            int expectedCount = 3;
+            int actualCount = CountWhileOccurrences(testFilename);
+
+            // Перевірка результату
+            Assert::AreEqual(expectedCount, actualCount);
+        }
+
+        // Тестування на порожньому файлі
+        TEST_METHOD(TestEmptyFile)
+        {
+            std::string emptyFilename = "empty_file.txt";
+            std::ofstream emptyFile(emptyFilename);
+            emptyFile.close();
+
+            int expectedCount = 0;
+            int actualCount = CountWhileOccurrences(emptyFilename);
+
+            Assert::AreEqual(expectedCount, actualCount);
+        }
+
+        // Тестування на файлі без входжень "while"
+        TEST_METHOD(TestNoWhileOccurrences)
+        {
+            std::string noWhileFilename = "no_while_file.txt";
+            std::ofstream noWhileFile(noWhileFilename);
+            noWhileFile << "This is a test file\n";
+            noWhileFile.close();
+
+            int expectedCount = 0;
+            int actualCount = CountWhileOccurrences(noWhileFilename);
+
+            Assert::AreEqual(expectedCount, actualCount);
+        }
+    };
+}
